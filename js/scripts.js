492b21510c79e0be34664e29ef0688bbdf71654d
Dominik Magic <dominikmagic5@gmail.com>
2022-10-25 14:10:27 -0700

    It will detect bad words and remove them
    Co-authored-by: Luke Callahan  <lukemcal@gmail.com>
    Co-authored-by: VW <vera.weikel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,24 +24,39 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;               // else return = 0
   });
 }
+
+
 function omitBadWords(text) {
   // convert text to an array of items
   const textArray = text.split(" ");
 
   // declare a variable set equal to the bad word
-  const badWordArray =  "zoinks"; //,"muppeteer"," biffaroni","loopdaloop"];
- const displayArray = [ ];
-  // compare the text array to the bad word
-  textArray.forEach(function(element){
-    if (element.toLowerCase().includes(badWordArray.toLowerCase())){
-        // console.log("Look at this offensive word I found!: " + badWord);
-        let display = displayArray.push(element);
-        console.log("Display & displayArray rez: ", displayArray, display)
-        return display;
-    }
-  });
+  const badWordArray =  ["zoinks", "muppeteer","biffaroni","loopdaloop"];
+  // let displayArray = [ ];
 
+  // compare the text array to the bad word
+  badWordArray.forEach(function(element){
+    if(textArray.includes(element)) {
+      let index = textArray.indexOf(element);
+      textArray.splice(index, 1);
+    }
+    return textArray;
+  });
+  console.log(textArray);
   // if there's a match, omit the bad word (.remove()) .concat or .push
   // rejoin the text array and display it
-
 }
+
+// if (element.includes(badWordArray)){
+
+//       if (element, text) {
+//         if (element === text) {
+//           return element++;
+//         }
+//         return element;
+//       }
+//         // console.log("Look at this offensive word I found!: " + badWord);
+//         displayArray.push(element);
+//         console.log("Display & displayArray rez: ", displayArray);
+//         return display;
+//     }
